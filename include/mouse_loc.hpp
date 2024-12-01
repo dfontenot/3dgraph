af9d0136a215bbb69777621d70f433243d6b6453
David Fontenot <davidmfontenot@gmail.com>
2024-11-30 21:55:12 -0800

    working on mouse location

--- include/mouse_loc.hpp
+++ include/mouse_loc.hpp
@@ -3,6 +3,8 @@
 #include <cmath>
 
 #include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <glm/vec2.hpp>
 
 class MouseLoc {
     int x;
@@ -20,5 +22,10 @@ public:
 
     double distance(const MouseLoc& other) const {
         return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
+    }
+
+    glm::vec2 unit_vec(const MouseLoc& other) const {
+        double dist = distance(other);
+        return glm::vec2((x - other.x) / dist, (y - other.y) / dist);
     }
 };
