69db1f6b73887b32c07bdbbb875f57ff0593039d
David Fontenot <davidmfontenot@gmail.com>
2025-05-16 14:52:10 -0700

    implemented 3d function
    output is still all constant color

--- shaders/tes.glsl
+++ shaders/tes.glsl
@@ -1,17 +1,20 @@
 #version 450 core
 
 // reference: https://learnopengl.com/Guest-Articles/2021/Tessellation/Tessellation
-layout (quads, fractional_odd_spacing, ccw) in;
+layout(quads, fractional_odd_spacing, ccw) in;
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
-out vec4 tes_color;
+// out vec4 tes_color;
 
 void main() {
     // reference: https://gamedev.stackexchange.com/a/87643
     vec4 p1 = mix(gl_in[0].gl_Position, gl_in[3].gl_Position, gl_TessCoord.x);
     vec4 p2 = mix(gl_in[1].gl_Position, gl_in[2].gl_Position, gl_TessCoord.x);
     vec4 interpolated = mix(p1, p2, gl_TessCoord.y);
+
+    // ref: https://www.benjoffe.com/code/tools/functions3d/examples
+    interpolated.z = mix(-0.5, 0.5, sin(10.0 * (pow(interpolated.x, 2) + pow(interpolated.y, 2))) / 10.0);
 
     gl_Position = u_projection * u_view * u_model * interpolated;
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -8,6 +8,6 @@ uniform float u_offset_y;
 uniform float u_offset_z;
 
 void main() {
-  gl_PointSize = 5.0f;
+  //gl_PointSize = 5.0f;
   gl_Position = vec4(position.x + u_offset_x, position.y + u_offset_y, position.z + u_offset_z, 1.0f);
 }
