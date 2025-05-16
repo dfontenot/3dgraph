202a15e8c474b5233ea5bc02232d82742db3df43
David Fontenot <davidmfontenot@gmail.com>
2025-05-16 16:41:31 -0700

    trying to apply checkerboard pattern to debug shaders
    background is still solid blue

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,6 +1,11 @@
 #version 410 core
 
-layout (location = 0) out vec4 frag_color;
+layout(location = 0) out vec4 frag_color;
+in vec2 uv;
+
 void main() {
-  frag_color = vec4(1.0, 0.0, 0.0, 1.0);
+    // ref: https://stackoverflow.com/a/64657127
+    vec2 repeat = vec2(5.0, 4.0); // repeat in x and y direction
+    float result = mod(dot(vec2(1.0), step(vec2(0.5), fract(uv * repeat))), 2.0);
+    frag_color = mix(vec4(0.0, 0.0, 1.0, 1.0), vec4(0.0, 1.0, 0.0, 1.0), result);
 }
--- shaders/tes.glsl
+++ shaders/tes.glsl
@@ -2,6 +2,14 @@
 
 // reference: https://learnopengl.com/Guest-Articles/2021/Tessellation/Tessellation
 layout(quads, fractional_odd_spacing, ccw) in;
+
+out vec2 uv;
+
+// ref: https://gist.github.com/companje/29408948f1e8be54dd5733a74ca49bb9
+float map(float value, float min1, float max1, float min2, float max2) {
+    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
@@ -14,7 +22,11 @@ void main() {
     vec4 interpolated = mix(p1, p2, gl_TessCoord.y);
 
     // ref: https://www.benjoffe.com/code/tools/functions3d/examples
+    // apply the function now that there are more levels of tessellation
     interpolated.z = mix(-0.5, 0.5, sin(10.0 * (pow(interpolated.x, 2) + pow(interpolated.y, 2))) / 10.0);
+
+    // before rotation, etc. store the UV coords to use in the fragment shader
+    uv = vec2(map(interpolated.x, -1.0, 1.0, 0.0, 1.0), map(interpolated.y, -1.0, 1.0, 0.0, 1.0));
 
     gl_Position = u_projection * u_view * u_model * interpolated;
 }
