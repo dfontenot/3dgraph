38986355297f7818531cfdffa30cb7699a067d9f
David Fontenot <davidmfontenot@gmail.com>
2025-05-16 17:15:45 -0700

    use maprange instead of mix

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,7 +5,7 @@ in vec2 uv;
 
 void main() {
     // ref: https://stackoverflow.com/a/64657127
-    vec2 repeat = vec2(5.0, 4.0); // repeat in x and y direction
+    vec2 repeat = vec2(20.0, 20.0); // repeat in x and y direction
     float result = mod(dot(vec2(1.0), step(vec2(0.5), fract(uv * repeat))), 2.0);
     frag_color = mix(vec4(0.0, 0.0, 1.0, 1.0), vec4(0.0, 1.0, 0.0, 1.0), result);
 }
